614fc7507eebcc5fccaf904b1634947b5f327976
Carlos2326-jpg <joaocarlosdutra2326@gmail.com>
2026-05-14 09:20:50 -0300

    Aula 03: Engenharia de Software

---
+++ Laboratorio_Engenharia-Software/2°Bimestre/Aula02/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <header className="Top">
+      <h1 className="Title">
+        Hello Word!
+      </h1>
+    </header>
+  );
+}
+
+export default App;
